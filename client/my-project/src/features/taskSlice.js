b5be9a0a8586bd31901e259fd5fef0b364bca4e0
rajtharani77 <117341499+rajtharani77@users.noreply.github.com>
2026-02-02 01:48:31 +0530

    Update API_URL to use environment variable

--- client/my-project/src/features/taskSlice.js
+++ client/my-project/src/features/taskSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/tasks/';
+const API_URL = `${import.meta.env.VITE_API_BASE_URL}/tasks/`;
 
 export const getTasks = createAsyncThunk('tasks/get', async (projectId, thunkAPI) => {
   try {
